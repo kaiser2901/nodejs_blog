f4d837cbd08f82b1d5b4a8f8dccba935f7fdf3a2
Le Tran Huu Nghi <ngondzai@gmail.com>
2021-12-17 13:53:00 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Sử dụng thư viện express
+const express = require('express')
+const app = express()
+//set port
+const port = 3000
+
+app.get('/', (req, res) => {
+  var a = 1;
+  var b = 2; 
+  var c = a + b;
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
