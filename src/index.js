16b44d3e0a916022c568442ff9b7ae5395074210
Le Tran Huu Nghi <ngondzai@gmail.com>
2021-12-18 13:05:34 +0700

    express handlebars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Sử dụng thư viện express
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template Engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+// console.log(__dirname)
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
