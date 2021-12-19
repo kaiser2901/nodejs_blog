40343d16aea1076c27a5f9faaa1a2bee4b142d1d
Le Tran Huu Nghi <ngondzai@gmail.com>
2021-12-19 12:25:09 +0700

    Static file eand SASS setting

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')));
+
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -15,7 +17,6 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
-// console.log(__dirname)
 app.get('/', (req, res) => {
   res.render('home');
 })
