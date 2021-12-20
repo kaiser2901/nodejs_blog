29e24cf681d6607141b7dbeb188f5c2464e23ff1
Le Tran Huu Nghi <ngondzai@gmail.com>
2021-12-20 16:00:47 +0700

    add search page 20/12/2021

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Sử dụng thư viện express
+// Sử dụng thư viện
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
@@ -9,20 +9,27 @@ const port = 3000;
 app.use(express.static(path.join(__dirname,'public')));
 
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template Engine
 app.engine('hbs', handlebars.engine({
   extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
+
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+app.get('/search', (req, res) => {
+  console.log(req.query);
+  res.render('search');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
