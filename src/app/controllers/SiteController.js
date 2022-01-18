4990924044445e62864510fb0ce3b67c79587918
Le Tran Huu Nghi <ngondzai@gmail.com>
2022-01-18 14:11:40 +0700

    add model, update SiteController

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,17 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /index
     index(req, res) {
-        res.render('home');
-    }
+        Course.find({}, function (err, courses) {
+            // docs.forEach
+            if (!err) {
+                res.json(courses);
+            } else {
+                res.status(400).json({ error: 'ERROR!'});
+            }
+        })
+    };
 
     // [GET] /search
     search(req, res) {
